ede406113b576a0145d295b43eaaf2ebf9d2b809
Ahmed Mohsen <ahmed7890@gmail.com>
2009-12-26 03:03:08 +0200

    Flight Added the implementation is over except the problem of reserve() and return() method OneWayFlight: Done TwoWayFligh: Done

--- src/java/items/Flight.java
+++ src/java/items/Flight.java
@@ -24,5 +24,139 @@ public abstract class Flight extends ReservationItem
     protected ArrayList<AgeGroup> myAgeGroup;
     protected DateInformation myDateInformation;
 
+    public Flight()
+    {
+        super();
+        sourceAirport = new Airport();
+        destinationAirport = new Airport();
+        economyTicketPrice = 0.0;
+        firstTicketPrice = 0.0;
+        bussinessTicketPrice = 0.0;
+        availableBussinessSeats = 0;
+        availableEconomySeats = 0;
+        availableFirstSeats = 0;
+        myAgeGroup = new ArrayList<AgeGroup>();
+    }
 
+    public Flight(int quantity, Airport sourceAirport, Airport destinationAirport, Double economyTicketPrice, Double firstTicketPrice, Double bussinessTicketPrice, int availableFirstSeats, int availableBussinessSeats, int availableEconomySeats, ArrayList<AgeGroup> myAgeGroup)
+    {
+        super(quantity);
+        this.sourceAirport = sourceAirport;
+        this.destinationAirport = destinationAirport;
+        this.economyTicketPrice = economyTicketPrice;
+        this.firstTicketPrice = firstTicketPrice;
+        this.bussinessTicketPrice = bussinessTicketPrice;
+        this.availableFirstSeats = availableFirstSeats;
+        this.availableBussinessSeats = availableBussinessSeats;
+        this.availableEconomySeats = availableEconomySeats;
+        this.myAgeGroup = myAgeGroup;
+    }
+
+    public int getAvailableBussinessSeats()
+    {
+        return availableBussinessSeats;
+    }
+
+    public void setAvailableBussinessSeats(int availableBussinessSeats)
+    {
+        this.availableBussinessSeats = availableBussinessSeats;
+    }
+
+    public int getAvailableEconomySeats()
+    {
+        return availableEconomySeats;
+    }
+
+    public void setAvailableEconomySeats(int availableEconomySeats)
+    {
+        this.availableEconomySeats = availableEconomySeats;
+    }
+
+    public int getAvailableFirstSeats()
+    {
+        return availableFirstSeats;
+    }
+
+    public void setAvailableFirstSeats(int availableFirstSeats)
+    {
+        this.availableFirstSeats = availableFirstSeats;
+    }
+
+    public Double getBussinessTicketPrice()
+    {
+        return bussinessTicketPrice;
+    }
+
+    public void setBussinessTicketPrice(Double bussinessTicketPrice)
+    {
+        this.bussinessTicketPrice = bussinessTicketPrice;
+    }
+
+    public Airport getDestinationAirport()
+    {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(Airport destinationAirport)
+    {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public Double getEconomyTicketPrice()
+    {
+        return economyTicketPrice;
+    }
+
+    public void setEconomyTicketPrice(Double economyTicketPrice)
+    {
+        this.economyTicketPrice = economyTicketPrice;
+    }
+
+    public Double getFirstTicketPrice()
+    {
+        return firstTicketPrice;
+    }
+
+    public void setFirstTicketPrice(Double firstTicketPrice)
+    {
+        this.firstTicketPrice = firstTicketPrice;
+    }
+
+    public ArrayList<AgeGroup> getMyAgeGroup()
+    {
+        return myAgeGroup;
+    }
+
+    public void setMyAgeGroup(ArrayList<AgeGroup> myAgeGroup)
+    {
+        this.myAgeGroup = myAgeGroup;
+    }
+
+    public Airport getSourceAirport()
+    {
+        return sourceAirport;
+    }
+
+    public void setSourceAirport(Airport sourceAirport)
+    {
+        this.sourceAirport = sourceAirport;
+    }
+
+
+    public void addAgeGroup(AgeGroup group)
+    {
+
+        myAgeGroup.add(group);
+    }
+
+    public boolean isInUse()
+    {
+        //assume that at the begining quantity =available number of all seats
+        if (quantity == availableBussinessSeats + availableEconomySeats + availableFirstSeats)
+        {
+            return false;
+        }
+        return false;
+
+    }
 }
